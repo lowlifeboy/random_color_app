a85b45a4aec19b8c7e1cd9be6f7c2d8844135e17
Konstantin Savchenko <killerkennyx41@gmail.com>
2019-07-14 02:25:58 +0300

    Add random colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 // import 'package:flutter/foundation.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Random Color App',
       theme: ThemeData(
-        primaryColor: Colors.blue,
+        primaryColor: Colors.white,
       ),
       home: RandomColor(),
     );
@@ -23,13 +25,49 @@ class RandomColor extends StatefulWidget {
 }
 
 class RandomColorState extends State<RandomColor> {
-  // String _color = '#fff';
+  final Random _random = Random();
 
-  // void _incrementCounter() {
-  //   setState(() {
-  //     _color = '#fff';
-  //   });
-  // }
+  Color _color = Color(0xFFFFFFFF);
+
+  int r = 0;
+  int g = 0;
+  int b = 0;
+  double o = 0;
+
+  void changeColor() {
+    setState(() {
+      r = _random.nextInt(255);
+      g = _random.nextInt(255);
+      b = _random.nextInt(255);
+      o = (_random.nextDouble() * 100).round() / 100;
+      _color = Color.fromRGBO(r, g, b, o);
+    });
+  }
+
+  Widget _currentColor(r, g, b, o, _color) {
+    return new Container(
+      decoration: new BoxDecoration(
+        color: _color,
+      ),
+      child: new Center(
+        child: new Container(
+          child: new Text('Hey there\n\nrgba($r, $g, $b, $o)'),
+          padding: const EdgeInsets.all(8.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(5.0),
+            boxShadow: [
+            const BoxShadow(
+              offset: const Offset(2.0, 2.0),
+              blurRadius: 2.0,
+              spreadRadius: -1.0,
+              color: const Color(0x44000000)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,25 +75,10 @@ class RandomColorState extends State<RandomColor> {
       appBar: AppBar(
         title: Text('Random Color App'),
       ),
-      
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Hey there',
-            ),
-          ],
-        ),
-      ),
-      backgroundColor: Color.fromRGBO(220, 245, 245, 1),
-      
-      
-      // floatingActionButton: FloatingActionButton(
-        // onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: InkWell(
+        onTap: changeColor,
+        child: _currentColor(r, g, b, o, _color),
+      )
     );
   }
 }
